b71bd82566d0322de6e1aa9ab8870657b8151a82
cphalen <=>
2018-02-03 15:59:56 -0600

    Ready IDCard with data for Winter Formal 2018

--- IDCard.py
+++ IDCard.py
@@ -2,7 +2,7 @@ from sharedCode import send
 
 print("Type 'exit' to exit the program")
 
-f = open('ISO-Dump.csv')
+f = open('Gatsby-winter-formal.csv')
 
 lines = f.read().split("\r")
 
@@ -16,13 +16,13 @@ for line in lines:
 
 while True:
     input_var = str(raw_input("Scan ID: "))
-    
+
     if input_var == 'exit':
         exit()
-    
+
     RFIDNum = input_var
-    
+
     for key in RFID:
         if(RFIDNum in key):
+            print RFID[key]
             send(RFID[key])
-    
